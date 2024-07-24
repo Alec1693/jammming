bcd4ddf59e3f195a042a95a5a524f4e69728971f
Alec1693 <alecchristiansen@gmail.com>
2024-07-24 16:33:58 -0400

    7/24Update

--- src/searchResults.js
+++ src/searchResults.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 //section to display results of search
 //map through array of song objects and display them in this section
 //assign id to divs for each song list created
-export default function SearchResults(props){
+export default function SearchResults({songList, addToPlayList}){
     //surely I will use useState to keep track of the tracks added from search results? Can I store it in the same loop as assigning divs?
     return (
         //need to add a clickhandler which updates state with newly added song
         <div>
             <h2>Results</h2>
-            {props.songs.map(song => {
+            {songList.songs.map(song => {
                 if(!song.isAdded){
                     return (
                         <div id={song.id}>
                             <p>{song.trackName}</p>
                             <p>{song.artistName}</p>
                             <p>{song.albumName}</p>
-                            <button className="addButton">+</button>
+                            <button onClick={() => addToPlayList(songList.song.isAdded = true)} className="addButton">+</button>
                         </div>
                     )
                 }
--- src/songsDisplay.js
+++ src/songsDisplay.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchResults from './searchResults';
 import Playlist from './playlist';
 
 
 export default function SongDisplay(props){
+    const [songList, setSongList] = useState(props.data);
     return (
         //pass the props to both components and let the components determine whether to render or not based on key in song object
         <div>
-            <SearchResults songs={props.data}/>
-            <Playlist songs={props.data}/>
+            <SearchResults songs={songList} addToPlaylist={(songList) => setSongList({songList})}/>
+            <Playlist songs={songList}/>
         </div>
     )
 }
