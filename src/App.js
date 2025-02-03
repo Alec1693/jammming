b1a57716a98a18f79797e4b05e8273fabec12b11
Alec Christiansen <alecchristiansen@gmail.com>
2025-02-03 16:20:08 -0500

    update.2.3.25

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import SearchBar from "./searchbar";
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  //state stores results from search via spotify api
+  const [searchResults, setSearchResults] = useState([]);
+  //callback function to retrieve the search results from SearchBar
+  const storeSearchResults = (results) => {
+    setSearchResults(results);
+  }
+
+  const test = (t) => {
+    console.log(t)
+  }
   //3 react components needed for app: Searchbar, Results, Playlist
   //1 Module to process API requests: Spotify
 
   return (
     <div>
-      <SearchBar />
+      <SearchBar storeSearch={storeSearchResults}/>
     </div>
   );
 }
--- src/searchbar.js
+++ src/searchbar.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 
-function SearchBar(){
+function SearchBar(props){
     const [search, setSearch] = useState("");
 
     const handleSearch = (e) => {
         e.preventDefault();
         console.log("You pressed enter!");
+        //should I call the spotify module here and store the search results in a state in Searchbar or use a callback function to app and store in state there?
+        //I think using a callback here to the app component and storing the state in app is best bc I will need to send data to sibling component
+        //const searchResults = 
+        //callback function to app
+        props.storeSearch([["Josh Turner", "Long black train", "picURL:3341", "songID:444"],["Beastie Boys", "Paul Revere", "picURL:6509", "songID:552"]]);
     }
 
 
     return (
         <div>
             <form onSubmit={handleSearch}>
-                <input type="text" onChange={(e) => setSearch(e.target.value)} value={search} id="search"></input>
+                <input placeholder="Search for artist..." type="text" onChange={(e) => setSearch(e.target.value)} value={search} id="search"></input>
             </form>
         </div>
     )
